2615f12d6af17699309a6bc909685007c177a863
Hindax <haar162@more2learn.de>
2022-11-15 10:26:37 +0100

    Arrays position

--- script_01.js
+++ script_01.js
@@ -2,10 +2,53 @@
 
 /* Die Satzbau-Maschine | Arrays */
 
+/**Theorie: Array */
+
+let arr;
+// arr = new Array(); // Konstruktor
+arr = []; // Literal
+arr = [2,7,11,4];
+arr = [false,true,false];
+arr = ["Ich", "bin", "Max", "Mütze"];
+
+output(arr);
+output(arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0 (1. pos. in Array)
+// output(arr[3]); // Konkret
+output(arr[arr.length -1]); // letzte Position allgemein
+
+// ()-- Parameters, Tests
+// {}-- Codeblöcke
+// [] -- Arrays, Index
+
+
+
+
+
+
+
+
 
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
+// output(getSentence("Ich", "bin", "Max", "Mütze"));
+// output(getSentence("Ich", "bin", "der", "coole", "Max", "Mütze"));
+// output(getSentence("Ich", "bin", "die", "coole", "Maxine", "Mützerin"));
+// function getSentence(word1,word2,word3,word4,word5,word6) {
+
+//     const gap   = " ";
+//     const punkt = ".";
+//     let str     = word1 + gap +
+//                   word2 + gap +
+//                   word3 + gap +
+//                   word4 + gap +
+//                   word5 + gap +
+//                   word6 +
+//                   punkt;
+
+//     return str;
+// }
 
 
 // Modul: Ausgabe in Konsole : Test
