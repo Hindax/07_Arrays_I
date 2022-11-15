78582c8bbf52ad3c5ec219dfb9e1ac0e301419e2
Hindax <haar162@more2learn.de>
2022-11-15 10:42:02 +0100

    01a Finktionnen mit Array

--- script_01.js
+++ script_01.js
@@ -4,18 +4,18 @@
 
 /**Theorie: Array */
 
-let arr;
-// arr = new Array(); // Konstruktor
-arr = []; // Literal
-arr = [2,7,11,4];
-arr = [false,true,false];
-arr = ["Ich", "bin", "Max", "Mütze"];
+// let arr;
+// // arr = new Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,7,11,4];
+// arr = [false,true,false];
+// arr = ["Ich", "bin", "Max", "Mütze"];
 
-output(arr);
-output(arr.length); // Anzahl der Elemente
-output(arr[0]); // Index 0 (1. pos. in Array)
-// output(arr[3]); // Konkret
-output(arr[arr.length -1]); // letzte Position allgemein
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. pos. in Array)
+// // output(arr[3]); // Konkret
+// output(arr[arr.length -1]); // letzte Position allgemein
 
 // ()-- Parameters, Tests
 // {}-- Codeblöcke
@@ -27,28 +27,32 @@ output(arr[arr.length -1]); // letzte Position allgemein
 
 
 
-
-
-
 /*** 01. Funktionalität mit Einzelparametern */
+function getSentence(word1,word2,word3,word4,word5,word6) {
+    return str;
+}
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich", "bin", "Max", "Mütze"));
 // output(getSentence("Ich", "bin", "der", "coole", "Max", "Mütze"));
 // output(getSentence("Ich", "bin", "die", "coole", "Maxine", "Mützerin"));
-// function getSentence(word1,word2,word3,word4,word5,word6) {
 
-//     const gap   = " ";
-//     const punkt = ".";
-//     let str     = word1 + gap +
-//                   word2 + gap +
-//                   word3 + gap +
-//                   word4 + gap +
-//                   word5 + gap +
-//                   word6 +
-//                   punkt;
+/** 01a Finktionnen mit Array */
+// Kritik, Überlegung
 
-//     return str;
-// }
+output(getSentenceArr(["Ich", "bin", "die", "coole", "Maxine", "Mützerin"]))
+function getSentenceArr(arr) {
+    const GAP   = " ";
+    const punkt = ".";
+    let str     = arr[0] + GAP +
+                  arr[1] + GAP +
+                  arr[2] + GAP +
+                  arr[3] + GAP +
+                  arr[4] + GAP +
+                  arr[5] +
+                  punkt;
+
+    return str;
+}
 
 
 // Modul: Ausgabe in Konsole : Test
