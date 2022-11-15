b145c14dbb726374fe536849985bba4fd66a0469
Hindax <haar162@more2learn.de>
2022-11-15 12:31:32 +0100

    to check

--- script_01.js
+++ script_01.js
@@ -40,6 +40,42 @@
 //     output("index i: " + i);
 // }
 
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 2 + 1
+// output("inhalt von a: " + a);
+
+
+/* Besser: mit FOR-Schleife */
+
+let a = 0; // Anfangswert
+
+for (let i = 0; i < 10; i++) {
+
+   output("in der loop:" + a);
+
+   a = a + 1;
+
+}
+
+output("nach der loop: " + a);
+
+
+
 /* 02b. For-Schleife für Array-Index (Iteration)*/
 let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
 for (let i = 0; i < arr.length; i++) {
@@ -86,6 +122,8 @@ function getSentenceArr(arr) {
     return str;
 }
 
+
+/** 01b. Funktionalität mit Array 1 */
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(outputStr) {
